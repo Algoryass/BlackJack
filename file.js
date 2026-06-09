42559431ec746464a30a72b4298877a7e27afbfc
IlyassCod <boukedirilyass486@gmail.com>
2026-06-09 11:14:33 +0200

    Add files

---
+++ file.js
@@ -0,0 +1,27 @@
+let firstCard = 10
+let secondCard = 13
+let sum = firstCard + secondCard
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+//let sumEl = document.getElementById("sum-el")
+let sumEl = document.querySelector("#sum-el") // query Selector instead of getElementById
+console.log(messageEl)
+
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card"
+    } else if (sum === 21) {
+        message = "You've got blackjack"
+    } else {
+        message = "You're out of the game"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    console.log(message)
+
+}
+
